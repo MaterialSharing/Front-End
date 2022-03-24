4f34bb069638a85f86a4ec011e2d21837e28262c
cxxu_11 <838808930@qq.com>
2022-03-24 20:09:58 +0800

    create Front-end Project

---
+++ ELA/miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-1gjyq6l2c1457bb2","alias":"cloud1"},{"envId":"prod-9gjzsnm5ec92ea82","alias":"prod"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
